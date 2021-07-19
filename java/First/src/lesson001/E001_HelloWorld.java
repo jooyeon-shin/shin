bf965803464d72f0cf91aca8b007309cc82f2e73
jooyeon-shin <87465062+jooyeon-shin@users.noreply.github.com>
2021-07-19 15:45:11 +0900

    2021-07-19 after class

---
+++ java/First/src/lesson001/E001_HelloWorld.java
@@ -0,0 +1,10 @@
+package lesson001;
+
+public class E001_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
---
+++ java/First/src/lesson001/E006_IfStmt.java
@@ -0,0 +1,17 @@
+package lesson001;
+
+public class E006_IfStmt {
+
+	public static void main(String[] args) {
+		// If statement : 조건이 만족할 때 코드블록을 실행한다
+		//                만족하지 않으면 코드블
+		// If Syntax
+		// if(조건) {...}
+		int age = 17;
+		if( age >=20) {
+            System.out.println("성년");
+        }
+        System.out.println("프로그램 종료...");
+	}
+
+}
